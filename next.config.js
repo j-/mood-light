3f2df747efa0e5166cf5d180992e9c54079ce6e5
j- <j@skeoh.com>
2021-06-17 20:13:12 +1000

    Revert "Add asset prefix"
    This reverts commit 6041fa95e2b3ebc78bc2684a46552ef9dd9c2b31.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const { parse } = require('url');
 const { homepage } = require('./package.json');
 
-const { pathname } = parse(homepage);
-
 module.exports = {
-  assetPrefix: pathname,
-  basePath: pathname,
+  basePath: parse(homepage).pathname,
 };
